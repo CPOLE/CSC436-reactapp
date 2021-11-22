7043439007b24d204976d949ecd07c990a723f89
CPOLE <CPOLE@depaul.com>
2021-11-22 08:20:28 -0500

    checking in

--- src/todo/Todo.js
+++ src/todo/Todo.js
@@ -4,33 +4,32 @@ import {useResource} from "react-request-hook"
 import {Link} from "react-navi"
 import {Card, Button, Form} from "react-bootstrap"
 
-export default function Todo ( {title, author, description, todoId, short=false} ) {
+export default function Todo ( {title, author, description, todoId, completed, dateCompleted, short=false} ) {
 
     const {dispatch} = useContext(StateContext)
 	//	get rid of Set methods
-    const [completed, setCompleted] = useState(true)
-    const [dateCompleted, setDateCompleted] = useState(Date.now())
-    const [toggled, toggleTodo] = useResource( () => ( {
-        url: `/todos/${encodeURI(todoId)}`,
+    const [toggled, toggleTodo] = useResource( (todoId, completed) => ( {
+        url: `/todos/${todoId}`,
         method: "patch",
-        data: {completed, dateCompleted}
+        data: {completed: completed, dateCompleted}
     }))
-    useEffect(() => {
-		//	get rid of Set method
-        setDateCompleted(Date.now);
-		//if (deleted && deleted .data && deleted.isLoading === false) ... then dispatch
-        dispatch( {type: "TOGGLE_TODO", todoId: todoId, completed: completed, dateCompleted: dateCompleted} )
-    }, [toggled])
-    //
-    const [deleted, deleteTodo] = useResource( () => ( {
-        url: `/todos/${encodeURI(todoId)}`,
+
+    const [deleted, deleteTodo] = useResource( (todoId) => ( {
+        url: `/todos/${todoId}`,
         method: "delete"
     }))
-    // useEffect(() => {
-        // if (deleted && deleted.data && deleted.isLoading === false) {
-            // dispatch({type: 'DELETE_TODO', todoId: todoId})
-        // }
-    // }, [deleted])
+	
+	useEffect(() => {
+        if (toggled && toggled.data && toggled.isLoading === false) {
+        dispatch( {type: "TOGGLE_TODO", todoId, completed: completed: toggled.data.completed, dateCompleted: toggled.data.dateCompleted} )
+    }, [toggled])
+    
+	
+    useEffect(() => {
+        if (deleted && deleted.data && deleted.isLoading === false) {
+            dispatch({type: 'DELETE_TODO', todoId: todoId})
+        }
+    }, [deleted])
 
     let processedDescription = description
     if (short) {
